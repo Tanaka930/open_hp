bd1bfb6bee24f4f54241d2ef59ffe396c48d8a2a
Tanaka930 <yumat9637@gmail.com>
2021-11-13 16:56:46 +0900

    api連携

---
+++ src/libs/client.js
@@ -0,0 +1,7 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+  serviceDomain: process.env.NEXT_PUBLIC_MICRO_CMS_SERVICE_ID,
+  apiKey: process.env.NEXT_PUBLIC_MICRO_CMS_API_KEY,
+});
+

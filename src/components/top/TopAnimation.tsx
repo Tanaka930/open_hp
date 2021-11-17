2fb1bed095b73da838fb1f4ec1343f89fd581bd6
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-17 16:01:36 +0900

    アニメーション追加

--- src/components/top/TopAnimation.tsx
+++ src/components/top/TopAnimation.tsx
@@ -1,9 +1,21 @@
 import styles from '@/components/top/top.module.css'
+import topTextStyles from '@/components/top/topText.module.css'
 
 
 export default function TopAnimation(){
   return(
     <div className={styles.top}>
+      <span className={topTextStyles.topTextNo1}>
+        <span className={topTextStyles.topTextYuor}>あなた</span>の価値を
+      </span>
+      <br/>
+      <span className={topTextStyles.topTextNo2}>
+        全ての場所に
+      </span>
+      <br />
+      <span className={topTextStyles.topTextEn}>
+        - Branding your value everywhere -
+      </span>
       <div className={styles.wrap}>
         <div className={styles.c}></div>
         <div className={styles.c}></div>

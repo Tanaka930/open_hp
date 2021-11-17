9a9c919634efa7092cec75ce301710f3e3fe91f2
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-17 18:05:45 +0900

    コンポーネント切り分け

--- src/components/top/TopAnimation.tsx
+++ src/components/top/TopAnimation.tsx
@@ -1,21 +1,11 @@
 import styles from '@/components/top/top.module.css'
-import topTextStyles from '@/components/top/topText.module.css'
+import TopText from '@/components/top/TopText'
 
 
 export default function TopAnimation(){
   return(
     <div className={styles.top}>
-      <span className={topTextStyles.topTextNo1}>
-        <span className={topTextStyles.topTextYuor}>あなた</span>の価値を
-      </span>
-      <br/>
-      <span className={topTextStyles.topTextNo2}>
-        全ての場所に
-      </span>
-      <br />
-      <span className={topTextStyles.topTextEn}>
-        - Branding your value everywhere -
-      </span>
+      <TopText />
       <div className={styles.wrap}>
         {/* 300回ループを回す */}
         {[...Array(300)].map((_,index)=>(

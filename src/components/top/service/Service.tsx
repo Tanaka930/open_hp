54e933027de999fc23cb5ab20ef74b5f0f39dc2f
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-18 13:51:08 +0900

    css情報追加

--- src/components/top/service/Service.tsx
+++ src/components/top/service/Service.tsx
@@ -3,18 +3,19 @@ import Image from 'next/image'
 export default function Service(){
   return(
     <>
+    {/* #F6F7F7 */}
     <section>
-      <h2>Services</h2>
+      <h2 className='text-5xl w-full text-center my-10'>Services</h2>
       <h3>オープンストアは関わるすべての人を笑顔にします!!</h3>
-      <div>
+      <div className='float-left'>
         <Image src="/images/top/service/EC.png" width={300} height={200} alt="オープンストアEC関連事業" />
         <h4>EC</h4>
       </div>
-      <div>
+      <div className='float-left'>
         <Image src="/images/top/service/LINE.png" width={300} height={200} alt="オープンストアLINE関連事業" />
         <h4>LINE</h4>
       </div>
-      <div>
+      <div className='float-left'>
         <Image src="/images/top/service/DX.png" width={300} height={200} alt="オープンストア店舗DX関連事業" />
         <h4>店舗DX</h4>
       </div>

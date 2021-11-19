c63021fcc0566307e220d36faf734c9a39583fe4
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-19 17:40:26 +0900

    ニュース詳細画面のリンク作成

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -11,7 +11,10 @@ export const pagesPath = {
     $url: (url?: { hash?: string }) => ({ pathname: '/contact' as const, hash: url?.hash })
   },
   news: {
-    $url: (url?: { hash?: string }) => ({ pathname: '/news' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({ pathname: '/news' as const, hash: url?.hash }),
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/news/[id]' as const, query: { id }, hash: url?.hash })
+    })
   },
   recruit: {
     $url: (url?: { hash?: string }) => ({ pathname: '/recruit' as const, hash: url?.hash })

65a7405f98bbcb7a868e434c5d3e5dbc2125a750
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-23 15:36:12 +0900

    サービス部分対応

--- src/components/top/service/Service.tsx
+++ src/components/top/service/Service.tsx
@@ -5,24 +5,24 @@ export default function Service(){
   return(
     <>
     {/* オリジナルのカラーパレットの設定まだしていないです */}
-    <section className='bg-gray-100 h-screen w-full'>
-      <h2 className='text-7xl w-full text-center pt-24 pb-10'>Services</h2>
-      <h3 className='text-1xl w-full text-center py-10'>オープンストアは関わるすべての人を笑顔にします!!</h3>
-      <div className='w-full text-center space-x-8 flex justify-center py-18'>
-        <div className='float-left'>
+    <section className='bg-gray-100 h-auto w-full'>
+      <h2 className='text-4xl md:text-7xl w-full text-center pt-2 md:pt-8 pb-2 md:pb-4'>Services</h2>
+      <h3 className='text-lg w-full text-center py-10'>オープンストアは関わるすべての人を笑顔にします!!</h3>
+      <div className='w-full text-center md:space-x-8 md:flex md:justify-center py-18'>
+        <div className='md:float-left'>
           <Image src="/images/top/service/EC.png" width={300} height={200} alt="オープンストアEC関連事業" />
           <h4>EC</h4>
         </div>
-        <div className='float-left'>
+        <div className='md:float-left'>
           <Image src="/images/top/service/LINE.png" width={300} height={200} alt="オープンストアLINE関連事業" />
           <h4>LINE</h4>
         </div>
-        <div className='float-left'>
+        <div className='md:float-left'>
           <Image src="/images/top/service/DX.png" width={300} height={200} alt="オープンストア店舗DX関連事業" />
           <h4>店舗DX</h4>
         </div>
       </div>
-      <div className='mt-24'>
+      <div className='mt-12 md:mt-24'>
         <ReadMoreButton href="/about" />
       </div>
     </section>

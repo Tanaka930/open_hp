6b411fa4aaa66a821b0066c35c644ac8fa468edf
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-23 14:33:10 +0900

    sass導入

--- src/components/top/TopText.tsx
+++ src/components/top/TopText.tsx
@@ -1,4 +1,4 @@
-import topTextStyles from '@/components/top/css/topText.module.css'
+import topTextStyles from '@/components/top/css/topText.module.scss'
 
 export default function TopText(){
   return(

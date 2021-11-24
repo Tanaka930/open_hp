aeebb194b9307c0da412b3191e6920bf35b87965
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-24 18:05:34 +0900

    スマホ対応コミット漏れ

--- src/components/layout/LowerTopText.tsx
+++ src/components/layout/LowerTopText.tsx
@@ -6,7 +6,7 @@ export default function LowerTopText(props: Props){
 
   return(
     <>
-    <div className='flex items-center justify-center my-24'>
+    <div className='flex items-center justify-center mt-12 md:my-24'>
       <span className='text-2xl md:text-4xl'>
         {props.text}
       </span>

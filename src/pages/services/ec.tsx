91f93f8accd67f6d8dbaf5c33126866cd6e3edbb
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-24 12:38:05 +0900

    事業内容のコンポーネント追加

--- src/pages/services/ec.tsx
+++ src/pages/services/ec.tsx
@@ -2,15 +2,20 @@ import ButtonLink from '@/components/links/ButtonLink';
 import Seo from '@/components/Seo';
 import TopContent from '@/components/layout/TopContent';
 
+// トップテキスト用のコンポーネント
+import TopText from '@/components/layout/ LowerTopText'
+
 
 export default function Home(){
   return(
     <>
       <Seo templateTitle='オープンストアEC事業' />
       <TopContent bg="bg-ec_top" title="EC" />
-      <ButtonLink className='mt-6' href='/components' variant='light'>
+      <TopText text='事業内容' />
+      {/* <ButtonLink className='mt-6' href='/components' variant='light'>
         See all components
-      </ButtonLink>
+
+      </ButtonLink> */}
     </>
   )
 }

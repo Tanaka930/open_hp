c0d03447ca099fe629dc48ab02824f6e8d05d498
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-27 11:22:55 +0900

    ページネーション追加

--- src/components/layout/Pagination.tsx
+++ src/components/layout/Pagination.tsx
@@ -13,14 +13,22 @@ export default function Pagination(props: Props){
     [...Array(end - start + 1)].map((_, i) => start + i)
 
   return(
-    <ul>
+    <ul className="flex items-center justify-center">
+      <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg">
+        <a className="flex items-center font-bold" href="#">
+            <span className="mx-1">previous</span>
+        </a>
+      </li>
       {range(1, Math.ceil(props.totalCount / PER_PAGE)).map((number:number, index:number) => (
-        <li key={index}>
-          <Link href={ `${props.pathName}/page/${number}`}>
-            <a>{number}</a>
-          </Link>
+        <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+          <a className="font-bold" href={ `${props.pathName}/page/${number}`}>{number}</a>
         </li>
       ))}
+      <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+        <a className="flex items-center font-bold" href="#">
+            <span className="mx-1">Next</span>
+        </a>
+      </li>
     </ul>
   );
 

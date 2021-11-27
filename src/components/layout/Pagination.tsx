b037f1020c81ac23ce89944bf50bc775d40aa288
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-11-27 14:23:13 +0900

    中飛ばし処理

--- src/components/layout/Pagination.tsx
+++ src/components/layout/Pagination.tsx
@@ -22,7 +22,7 @@ export default function Pagination(props: Props){
     <ul className="flex items-center justify-center">
       <PreviousPage pathName={props.pathName} pageNum={props.pageNum} />
       {range(1, Math.ceil(props.totalCount / PER_PAGE)).map((number:number, index:number) => (
-        <TextColor pageNum={props.pageNum} num={number} pathName={props.pathName} />
+        <TextColor pageNum={props.pageNum} num={number} pathName={props.pathName} totalCount={props.totalCount} pre_page={PER_PAGE} />
       ))}
       <NextPage pathName={props.pathName} pageNum={props.pageNum} totalCount={props.totalCount} pre_page={PER_PAGE} />
     </ul>
--- src/components/layout/PaginationColor.tsx
+++ src/components/layout/PaginationColor.tsx
@@ -1,7 +1,9 @@
 type Props ={
   num:number;
   pageNum: number;
-  pathName: string
+  pathName: string;
+  totalCount: number;
+  pre_page: number;
 }
 
 export default function TextColor(props: Props){
@@ -12,11 +14,33 @@ export default function TextColor(props: Props){
       </li>
     );
   }else{
-    return(
-      <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
-        <a className="font-bold" href={ `${props.pathName}/page/${props.num}`}>{props.num}</a>
-      </li>
-    );
+    if(Math.abs(props.pageNum- props.num) <= 4){
+      return(
+        <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+          <a className="font-bold" href={ `${props.pathName}/page/${props.num}`}>{props.num}</a>
+        </li>
+      );
+    }else{
+      if(props.num == 1){
+        return(
+          <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+            <a className="font-bold" href={ `${props.pathName}/page/${props.num}`}>{props.num}</a>
+          </li>
+        );
+      }else{
+        if((props.totalCount / props.pre_page) <= props.num){
+          return(
+            <li className="mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg">
+              <a className="font-bold" href={ `${props.pathName}/page/${props.num}`}>{props.num}</a>
+            </li>
+          );
+        }else{
+          return(
+            <>
+            </>
+          );
+        }
+      }
+    }
   }
-
 }

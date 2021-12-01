9adfb5c59583a76cece3108cabec98dfd7dff407
Tanaka930 <yumat9637@gmail.com>
2021-12-01 18:38:26 +0900

    hanberger雛形作成

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,30 +2,22 @@ import React, {useState} from 'react';
 import UnstyledLink from '../links/UnstyledLink';
 // import Menu from './Menu';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { HamburgerArrow } from 'react-animated-burgers'
 
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { ClassValue } from 'clsx';
 
-
-// const user = {
-//   name: 'Tom Cook',
-//   email: 'tom@example.com',
-//   imageUrl:
-//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-// }
 
 const navigation = [
-  { name: 'Service', href: '/service', current: true },
-  { name: 'About', href: '/about', current: false },
+  { name: 'Service', href: '/service', current: false },
+  { name: 'About', href: '/about', current: false  },
   { name: 'News', href: '/news', current: false },
   { name: 'Recruit', href: '/recruit', current: false },
   { name: 'Contact', href: '/contact', current: false },
 ]
-// const userNavigation = [
-//   { name: 'Your Profile', href: '#' },
-//   { name: 'Settings', href: '#' },
-//   { name: 'Sign out', href: '#' },
-// ]
+
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -33,98 +25,118 @@ function classNames(...classes: string[]) {
 
 export default function Header() {
 
+  const router = useRouter();
+  console.log(router.pathname);
+
+  const [isActive, setIsActive] = useState(false);
+
+  const menuFunction = () => {
+    setIsActive(!isActive);
+  }
+
+  // const [openMenu, setOpenMenu] = useState(false);
+
+  // const menuFunction = () => {
+  //   setOpenMenu(!openMenu);
+  // }
+
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
-       <div className="absolute w-screen">
+      <div className="absolute w-screen">
         <Disclosure as="nav" className="">
-          {({ open }) => (
-            <>
-              <div className="max-w-screen-2xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-8 md:h-16">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Link href='/'>
-                        <img
-                          className="h-5"
-                          src="/images/layout/Logo.png"
-                          alt="openstore"
-                        />
-                      </Link>
-                    </div>
+          <>
+            <div className="max-w-screen-2xl mx-auto px-2 pt-4">
+              <div className="flex items-center justify-between h-8 md:h-16">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <Link href='/'>
+                      <img
+                        className="h-7 md:h-10"
+                        src="/images/layout/Logo.png"
+                        alt="openstore"
+                      />
+                    </Link>
                   </div>
+                </div>
 
-                  <div className="-mr-2 flex ">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className=" inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                <div className="-mr-2 flex ">
+                  {/* menu button */}
+                  <HamburgerArrow isActive={isActive} toggleButton={menuFunction} className="z-50" buttonColor="" barColor="#010305"/>
+                  {/* <Disclosure.Button className=" inline-flex items-center justify-center p-2 rounded-md text-gray-800 focus:outline-none">
+                    <span className="menu" onClick={() => menuFunction()}></span>
+                      {openMenu ? (
+                        <XIcon className="block h-8 w-8 md:h-12 md:w-12" aria-hidden="true" />
                       ) : (
-                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                        <MenuIcon className="block h-8 w-8 md:h-12 md:w-12" aria-hidden="true" />
                       )}
-                    </Disclosure.Button>
-                  </div>
+                  </Disclosure.Button> */}
                 </div>
               </div>
+            </div>
 
-              <Disclosure.Panel className="">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            {/* <Disclosure.Panel className="">
+              <div className="bg-green-600 z-50 px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block px-3 py-2 rounded-md text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </Disclosure.Panel> */}
+            {isActive ? (
+              <div className="z-40 fixed top-0 w-screen">
+                
+                <div className="bg-green-600 h-screen translate-x-0 transition-all duration-300 ease-linear z-40 px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <div className="h-12"></div>
                   {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-                {/* <div className="pt-4 pb-3 border-t border-gray-700">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
-                    </div>
-                    <button
-                      type="button"
-                      className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    >
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 px-2 space-y-1">
-                    {userNavigation.map((item) => (
-                      <Disclosure.Button
+                    <div>
+                      <a
                         key={item.name}
-                        as="a"
                         href={item.href}
-                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-white hover:bg-gray-700 hover:text-gray-300 ',
+                          'block px-3 py-2 rounded-md text-base font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
-                      </Disclosure.Button>
-                    ))}
-                  </div>
-                </div> */}
-              </Disclosure.Panel>
-            </>
-          )}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ):(
+              <div className=" z-40 fixed top-0 w-screen">
+                <div className="bg-green-600 h-screen translate-x-full transition-all duration-300 ease-linear z-40 px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <div className="h-12"></div>
+                  {navigation.map((item) => (
+                    <div>
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'block px-3 py-2 rounded-md text-base font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </>
         </Disclosure>
       </div>
     </>

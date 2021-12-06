846c3e8eb8680c467c0d4ffdb18b45b11172e715
Tanaka930 <yumat9637@gmail.com>
2021-12-06 18:33:01 +0900

    animation

--- src/components/buttons/ReturnTopButton.tsx
+++ src/components/buttons/ReturnTopButton.tsx
@@ -1,15 +1,43 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useMedia } from 'use-media'
+import { animateScroll as scroll } from 'react-scroll'
+
+const Button = styled.a<{isButtonActive:boolean}>`
+  position: fixed;
+  right: 5px;
+  bottom: 20px;
+  height: 50px;
+  text-decoration: none;
+  font-weight: bold;
+  transform: ${({ isButtonActive }) => (isButtonActive ? 'translateY(0) rotate(90deg)' : 'translateY(100) rotate(90deg)')};
+  visibility: ${({ isButtonActive }) => (isButtonActive ? 'visible' : 'hidden')};
+  opacity: ${({ isButtonActive }) => (isButtonActive ? '1' : '0')};
+  transition: 1s;
+  font-size: 90%;
+  line-height: 1.5rem;
+  color: rgb(245, 158, 11);
+  padding: 0 0 0 35px;
+  border-top: solid 2px;
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: -1px;
+    left: 0px;
+    width: 15px;
+    border-top: solid 2px;
+    transform: rotate(35deg);
+    transform-origin: left top;
+  }
+`
 
 export default function ReturnTopButton() {
 
-  const [isButtonActive, setIsButtonActive] = useState(false)
+  const [isButtonActive, setIsButtonActive] = useState<boolean>(false);
 
-  const returnTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
+  const scrollToTop = () => {
+    scroll.scrollToTop({smooth: true})
   }
 
   useEffect(() => {
@@ -30,55 +58,17 @@ export default function ReturnTopButton() {
     }
   }
 
-  // const normalStyle = {
-  //   opacity: 0,
-  //   transition: '0.5s',
-  //   pointerEvents: 'none'
-  // }
-  // const activeStyle = {
-  //   opacity: 1,
-  //   transition: '0.5s'
-  // }
-  // const style = isButtonActive ? activeStyle : normalStyle
-
-  const Button = isButtonActive ?
-    styled.a`
-    opacity: 1;
-    transition: 0.5s;
-
-      position: fixed;
-      right: 5px;
-      bottom: 20px;
-      height: 50px;
-      text-decoration: none;
-      font-weight: bold;
-      transform: rotate(90deg);
-      font-size: 90%;
-      line-height: 1.5rem;
-      color: rgb(245, 158, 11);
-      padding: 0 0 0 35px;
-      border-top: solid 1px;
-      &::before {
-        content: "";
-        display: block;
-        position: absolute;
-        top: -1px;
-        left: 0px;
-        width: 15px;
-        border-top: solid 1px;
-        transform: rotate(35deg);
-        transform-origin: left top;
-      }`
-    :
-    styled.a`
-      opacity: 0;
-      transition: 0.5s;
-      pointerEvents: none;`
+  const isWide = useMedia({ maxWidth: "767px" });
 
   return (
-    <Button
-    onClick={returnTop}>
-    TOP
-  </Button>
+    <>
+      {isWide &&
+        <Button
+          isButtonActive={isButtonActive}
+          onClick={scrollToTop}>
+          TOP
+        </Button>
+      }
+    </>
   )
 }

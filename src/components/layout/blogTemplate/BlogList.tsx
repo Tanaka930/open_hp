52829e548d95ec5865f81b56af5ed24cf774e0dd
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-08 17:51:59 +0900

    ページ横幅変更

--- src/components/layout/blogTemplate/BlogList.tsx
+++ src/components/layout/blogTemplate/BlogList.tsx
@@ -54,7 +54,7 @@ export default function BlogList(props: Props){
         <main className="mt-10">
           <NewBlog BlogObject={props.newBlogList[0]} />
 
-          <div className="block lg:flex lg:space-x-2 lg:p-0 my-10 mx-20">
+          <div className="block lg:flex lg:space-x-2 lg:p-0 my-10 mx-10 md:mx-20">
 
             <div className="w-full lg:w-2/3 pb-3">
               <BlogListBox blogList={props.blogList} searchSt={props.searchSt}/>

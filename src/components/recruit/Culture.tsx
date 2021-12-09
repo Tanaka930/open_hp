80a7a102ef4b84f65ddd7edf3a679b9991853613
Tanaka930 <yumat9637@gmail.com>
2021-12-09 14:09:13 +0900

    job[id]重複css削除　その他微修正

--- src/components/recruit/Culture.tsx
+++ src/components/recruit/Culture.tsx
@@ -5,11 +5,6 @@ type Props = {
   pageData: any;
 }
 
-type Context = {
-    // モバイルか？
-    isMobileSite: boolean;
-}
-
 const mediaQueries = {
   mobile: '(max-width: 1023px)'
 };
@@ -19,35 +14,19 @@ export default function Culture(props: Props){
   const isMobileSite = useMedia(mediaQueries.mobile);
 
   const text:string = props.pageData.culture
-  if(isMobileSite){
-    return(
-      <>
+
+  return(
+    <>
       <section className='h-auto w-full'>
-        <h2 className='text-4xl'>社風</h2>
+        <h2 className='text-4xl font-bold'>社風</h2>
         <RightText 
-          image={props.pageData.cultureImage2.url}
+          image={isMobileSite ? props.pageData.cultureImage2.url : props.pageData.cultureImage.url }
           text={text}
-          width={props.pageData.cultureImage2.width}
-          height={props.pageData.cultureImage2.height}
+          width={isMobileSite ? props.pageData.cultureImage2.width : props.pageData.cultureImage.width }
+          height={isMobileSite ? props.pageData.cultureImage2.height : props.pageData.cultureImage.height }
           alt_text={text}
         />
       </section>
-      </>
-    );
-  }else{
-    return(
-      <>
-      <section className='h-auto w-full'>
-        <h2 className='text-4xl'>社風</h2>
-        <RightText 
-          image={props.pageData.cultureImage.url}
-          text={text}
-          width={props.pageData.cultureImage.width}
-          height={props.pageData.cultureImage.height}
-          alt_text={text}
-        />
-      </section>
-      </>
-    );
-  }
+    </>
+  );
 }
--- src/components/recruit/JobDetail.tsx
+++ src/components/recruit/JobDetail.tsx
@@ -14,29 +14,29 @@ export default function JobDetail(props: Props){
       <div className="bg-white px-4 w-auto">
         <div className="h-1 w-full mx-auto border-b"></div>
         <div className="transition hover:bg-green-50">
-          <div className="transition flex space-x-5 px-5 items-center h-16 text-left">
+          <div className="transition flex space-x-5 px-5 items-center h-16 text-left" onClick={() => setExpanded(!expanded)}>
               <h3 className='text-lg md:text-2xl w-full'>{props.jobData.categoryTitle}</h3>
-              <span className="fas fa-plus cursor-pointer"  onClick={() => setExpanded(!expanded)}>{!expanded && <span className="text-4xl">＋</span>}{expanded && <span className="text-3xl">ー</span>}</span>
+              <span className="fas fa-plus cursor-pointer" >{!expanded && <span className="text-4xl">＋</span>}{expanded && <span className="text-3xl">ー</span>}</span>
           </div>
           {expanded && 
             <div className="px-5 pt-0">
-                <p className="leading-6 w-full font-light">
-                  {props.jobData.text}
-                </p>
-                <ul className='space-y-4 text-base md:text-xl p-8'>
-                {props.jobData.jobDatas.map((data:any) => (
-                  <li>
-                    <Link href={`/jobs/${data.jobId}`}>
-                      <div>
-                        <a className='cursor-pointer'>
-                          {data.title}
-                        </a>
-                      </div>
-                    </Link>
-                  </li>
-                ))}
-                </ul>
-                {/* <button className="rounded-full bg-green-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">詳しく見る</button> */}
+              <p className="leading-6 w-full font-light">
+                {props.jobData.text}
+              </p>
+              <ul className='space-y-4 text-base md:text-xl p-8'>
+              {props.jobData.jobDatas.map((data:any) => (
+                <li>
+                  <Link href={`/jobs/${data.jobId}`}>
+                    <div>
+                      <a className='cursor-pointer'>
+                        {data.title}
+                      </a>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+              </ul>
+              {/* <button className="rounded-full bg-green-600 text-white font-medium font-lg px-6 py-2 my-5 ml-9">詳しく見る</button> */}
             </div>
           }
         </div>

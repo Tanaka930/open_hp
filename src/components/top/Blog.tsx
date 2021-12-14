495ebd8482f455a75c9529b49ff196d8eb090fa7
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-14 16:03:15 +0900

    不要なコード削除

--- src/components/top/Blog.tsx
+++ src/components/top/Blog.tsx
@@ -2,9 +2,6 @@ import UnstyledLink from '@/components/links/UnstyledLink'
 import Moment from 'react-moment'
 import ContentImage from '@/components/layout/listTemplate/ContentListImage'
 
-// ページネーション用のコンポーネント
-import  Pagination from '@/components/layout/listTemplate/Pagination';
-
 type Props = {
   blogs: any;
 }
@@ -17,21 +14,19 @@ export default function blog(props:Props){
         <div className="container px-5 pt-12 pb-24 mx-auto max-w-7x1">
           <div className="flex flex-wrap -m-4">
             {props.blogs.map((content:any) => (
-              
-                <div className="xl:w-1/3 md:w-1/2 p-4">
-                  <UnstyledLink href={`/blog/${content.id}`} >
-                    <div className="bg-white pt-2 pb-4 px-2 rounded-lg cursor-pointer">
-                      <ContentImage image= {content.thumbnail} altText={content.previewWord}/>
-                      <h3 className="tracking-widest text-green-600 text-xs font-medium">
-                        <Moment format="YYYY年MM月DD日">
-                          {content.createDay}
-                        </Moment>
-                      </h3>
-                      <h2 className="text-lg text-gray-900 font-bold mb-6">{content.title}</h2>
-                    </div>
-                  </UnstyledLink>
-                </div>
-              
+              <div className="xl:w-1/3 md:w-1/2 p-4">
+                <UnstyledLink href={`/blog/${content.id}`} >
+                  <div className="bg-white pt-2 pb-4 px-2 rounded-lg cursor-pointer">
+                    <ContentImage image= {content.thumbnail} altText={content.previewWord}/>
+                    <h3 className="tracking-widest text-green-600 text-xs font-medium">
+                      <Moment format="YYYY年MM月DD日">
+                        {content.createDay}
+                      </Moment>
+                    </h3>
+                    <h2 className="text-lg text-gray-900 font-bold mb-6">{content.title}</h2>
+                  </div>
+                </UnstyledLink>
+              </div>
             ))}
           </div>
         </div>

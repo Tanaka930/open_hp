5c31b6c63bbba1f1a54c5c246756860a050ef081
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-15 11:32:27 +0900

    雛形作成

--- src/components/top/Blog.tsx
+++ src/components/top/Blog.tsx
@@ -10,7 +10,7 @@ export default function blog(props:Props){
   return(
     <>
       <section className="bg-white body-font mx-6 md:mx-36">
-        <h2 className='text-4xl md:text-7xl w-full pb-10 text-left'>Tpics</h2>
+        <h2 className='text-4xl md:text-7xl w-full pb-10 text-left'>TOPIC</h2>
         <h3 className='text-xl md:text-3xl w-full pb-10 text-left font-bold text-green-700'>-トピックス-</h3>
 
         <div className="container px-5 pt-6 md:pt-12 pb-12 md:pb-24 max-w-7x1">
--- src/components/top/Service.tsx
+++ src/components/top/Service.tsx
@@ -9,29 +9,35 @@ export default function Service(props:Props){
   return(
     <>
     {/* オリジナルのカラーパレットの設定まだしていないです */}
-    <section className='bg-gray-50 h-auto w-full'>
-      <h2 className='text-4xl md:text-7xl w-full text-center'>Services</h2>
-      <h3 className='text-xl w-full text-center py-10'>オープンストアは関わる<br className="md:hidden"/>すべての人を笑顔にします!!</h3>
+    <section className='bg-gray-50 h-auto mx-6 md:mx-36'>
+      <h2 className='text-4xl md:text-7xl w-full text-center'>SERVICE</h2>
+      {/* <h3 className='text-xl w-full text-center py-10'>オープンストアは関わる<br className="md:hidden"/>すべての人を笑顔にします!!</h3> */}
       <div className='w-full text-center md:space-x-8 md:flex md:justify-center py-18'>
-        <div className='text-2xl md:text-3xl'>
+        <div className='text-2xl p-4 md:w-1/3 md:text-3xl rounded-3xl border-green-700 border-2'>
           <Image src={props.data.ecImage.url}
-                 width={300}
-                 height={200}
+                 width={150}
+                 height={150}
                  alt="オープンストアEC関連事業"
                  decoding="async"
           />
-          <h4 className="mt-3 mb-10">EC</h4>
+          <h4 className="mt-3 mb-5 text-green-700">Shopify構築</h4>
+          <div className="text-base px-8 py-2 text-left">
+            <p>
+              大切にしているのは「向き合うこと。一人ひとりの悩みや不安に寄り添う診療をします。
+            </p>
+          </div>
         </div>
-        <div className='text-2xl md:text-3xl'>
+        <div className='text-2xl md:text-3xl rounded-2xl'>
           <Image src={props.data.lineImage.url}
                  width={300}
                  height={200}
                  alt="オープンストアLINE関連事業"
                  decoding="async"
+                 className="object-cover"
           />
           <h4 className="mt-3 mb-10">LINE</h4>
         </div>
-        <div className='text-2xl md:text-3xl'>
+        <div className='text-2xl md:text-3xl rounded-2xl'>
           <Image src={props.data.dxImage.url}
                  width={300}
                  height={200}

228b427dfaed0d3518e2706aa38ae745dd33c3a2
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-15 12:06:00 +0900

    途中経過1

--- src/components/top/Service.tsx
+++ src/components/top/Service.tsx
@@ -1,56 +1,33 @@
 import Image from 'next/image'
 import ReadMoreButton from '@/components/buttons/readMoreButton'
 
+import ServiceContent from '@/components/top/service/serviceContent'
+
 type Props = {
   data: any;
 }
 
 export default function Service(props:Props){
+  const text1 = "大切にしているのは「向き合うこと。一人ひとりの悩みや不安に寄り添う診療をします。"
+  const text2 = "大切にしているのは「向き合うこと。一人ひとりの悩みや不安に寄り添う診療をします。"
+  const text3 = "大切にしているのは「向き合うこと。一人ひとりの悩みや不安に寄り添う診療をします。"
+
   return(
     <>
-    {/* オリジナルのカラーパレットの設定まだしていないです */}
-    <section className='bg-gray-50 h-auto mx-6 md:mx-36'>
-      <h2 className='text-4xl md:text-7xl w-full text-center'>SERVICE</h2>
-      {/* <h3 className='text-xl w-full text-center py-10'>オープンストアは関わる<br className="md:hidden"/>すべての人を笑顔にします!!</h3> */}
-      <div className='w-full text-center md:space-x-8 md:flex md:justify-center py-18'>
-        <div className='text-2xl p-4 md:w-1/3 md:text-3xl rounded-3xl border-green-700 border-2'>
-          <Image src={props.data.ecImage.url}
-                 width={150}
-                 height={150}
-                 alt="オープンストアEC関連事業"
-                 decoding="async"
-          />
-          <h4 className="mt-3 mb-5 text-green-700">Shopify構築</h4>
-          <div className="text-base px-8 py-2 text-left">
-            <p>
-              大切にしているのは「向き合うこと。一人ひとりの悩みや不安に寄り添う診療をします。
-            </p>
-          </div>
+    <section className='bg-gray-50 h-auto mx-6'>
+      <div className="md:mx-36">
+        <h2 className='text-4xl md:text-7xl w-full pb-10 text-left'>SERVICE</h2>
+        <h3 className='text-xl md:text-3xl w-full pb-10 text-left font-bold text-green-700'>- 事業内容 -</h3>
+        <div className='w-full text-center space-y-8 xl:space-y-0 xl:space-x-8 md:flex md:justify-center py-18'>
+          <ServiceContent image= {props.data.ecImage.url} title="Shopify構築" text = {text1}/>
+          <ServiceContent image= {props.data.lineImage.url} title="LINEサービス" text = {text2}/>
+          <ServiceContent image= {props.data.lineImage.url} title="DX事業" text = {text3}/>
         </div>
-        <div className='text-2xl md:text-3xl rounded-2xl'>
-          <Image src={props.data.lineImage.url}
-                 width={300}
-                 height={200}
-                 alt="オープンストアLINE関連事業"
-                 decoding="async"
-                 className="object-cover"
-          />
-          <h4 className="mt-3 mb-10">LINE</h4>
+        <div className='mt-12 md:mt-24'>
+          <ReadMoreButton href="/service">
+            Read More ...
+          </ReadMoreButton>
         </div>
-        <div className='text-2xl md:text-3xl rounded-2xl'>
-          <Image src={props.data.dxImage.url}
-                 width={300}
-                 height={200}
-                 alt="オープンストア店舗DX関連事業"
-                 decoding="async"
-          />
-          <h4 className="mt-3 mb-10">店舗DX</h4>
-        </div>
-      </div>
-      <div className='mt-12 md:mt-24'>
-        <ReadMoreButton href="/service">
-          Read More ...
-        </ReadMoreButton>
       </div>
     </section>
     </>

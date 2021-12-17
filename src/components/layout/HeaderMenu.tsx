e399c8016e125348aff0e5b50a94f8100644f7c2
Tanaka930 <yumat9637@gmail.com>
2021-12-17 12:04:35 +0900

    すまほ

--- src/components/layout/HeaderMenu.tsx
+++ src/components/layout/HeaderMenu.tsx
@@ -4,12 +4,12 @@ import Image from 'next/image';
 
 const navigation = [
   { name: 'HOME', href: '/' },
-  { name: 'SERVICE', href: '/service' },
-  { name: 'ABOUT', href: '/about'  },
   { name: 'NEWS', href: '/news' },
   { name: 'RECRUIT', href: '/recruit' },
-  { name: 'CONTACT', href: '/contact' },
   { name: 'BLOG', href: '/blog' },
+  { name: 'ABOUT', href: '/about'  },
+  { name: 'CONTACT', href: '/contact' },
+  { name: 'SERVICE', href: '/service' },
 ]
 
 
@@ -31,33 +31,33 @@ export default function HeaderMenu(props: Props) {
     <>
       {props.isActive ? (
         <div className="fixed z-10 top-0 w-screen">
-          <div className="bg-white h-screen translate-x-0 transition-all duration-300 ease-linear px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <div className="h-12"></div>
+          <div className="relative bg-white h-screen translate-x-0 transition-all duration-300 ease-linear px-9 py-16 space-y-1 sm:px-3">
+            <div className="h-16"></div>
             <div className="flex flex-wrap">
               {navigation.map((item) => (
-                <div key ={item.name} className="w-1/2 py-8 md:pt-11" onClick={()=>props.toggleButton()}>
+                <div key ={item.name} className="w-1/2 py-11 md:pt-11" onClick={()=>props.toggleButton()}>
                   <UnstyledLink
                     href={item.href}
                     aria-current={item.href === path ? 'page' : undefined}
                     className={classNames(
                       item.href === path ? 'text-gray-800 cursor-default' : ' hover:text-yellow-500 ',
-                      'block px-2 py-2 rounded-md text-xl font-medium md:text-6xl md:text-center'
+                      'block rounded-md text-2xl md:text-6xl md:text-center'
                     )}
                   >
                     <div className="flex items-center">
-                      <span className={`inline-block h-4 w-4 rounded-full mr-2 ${item.href === path && 'bg-green-500'} `}></span>
+                      <span className={` ${item.href === path && 'inline-block h-4 w-4 rounded-full mr-2 bg-green-500'} `}></span>
                       {item.name}
                     </div>
                   </UnstyledLink>
                 </div>
               ))}
             </div>
-            <div className="pb-48">
+            <div className="">
               <span>aikon</span>
             </div>
-            <div className="flex flex-row-reverse pr-7 mt-56">
+            <div className="absolute bottom-16 right-9">
               <Image
-                width={200} height={24} 
+                width={234.78} height={22.87} 
                 src="/images/layout/Logo.png"
                 alt="openstore-logo"
                 decoding="async"
@@ -67,33 +67,33 @@ export default function HeaderMenu(props: Props) {
         </div>
       ):(
         <div className="fixed top-0 w-screen pointer-events-none">
-          <div className="bg-white h-screen translate-x-full transition-all duration-300 ease-linear px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <div className="h-12"></div>
+          <div className="relative bg-white h-screen translate-x-full transition-all duration-300 ease-linear px-9 py-16 space-y-1 sm:px-3">
+          <div className="h-16"></div>
             <div className="flex flex-wrap">
               {navigation.map((item) => (
-                <div key ={item.name} className="w-1/2 py-8 md:pt-11" onClick={()=>props.toggleButton()}>
+                <div key ={item.name} className="w-1/2 py-11 md:pt-11" onClick={()=>props.toggleButton()}>
                   <UnstyledLink
                     href={item.href}
                     aria-current={item.href === path ? 'page' : undefined}
                     className={classNames(
                       item.href === path ? 'text-gray-800 cursor-default' : ' hover:text-yellow-500 ',
-                      'block px-2 py-2 rounded-md text-xl font-medium md:text-6xl md:text-center'
+                      'block rounded-md text-2xl font-medium md:text-6xl md:text-center'
                     )}
                   >
                     <div className="flex items-center">
-                      <span className={`inline-block h-4 w-4 rounded-full mr-2 ${item.href === path && 'bg-green-500'} `}></span>
+                      <span  className={` ${item.href === path && 'inline-block h-4 w-4 rounded-full mr-2 bg-green-500'} `}></span>
                       {item.name}
                     </div>
                   </UnstyledLink>
                 </div>
               ))}
             </div>
-            <div className="pb-48">
+            <div className="">
               <span>aikon</span>
             </div>
-            <div className="flex flex-row-reverse pr-7 pt-56">
+            <div className="absolute bottom-16 right-9">
               <Image
-                width={200} height={24} 
+                width={234.78} height={22.87} 
                 src="/images/layout/Logo.png"
                 alt="openstore-logo"
                 decoding="async"

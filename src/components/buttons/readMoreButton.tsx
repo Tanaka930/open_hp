deb9ed4c627eea3d745f9e62597d89bf123c3aa7
Tanaka930 <yumat9637@gmail.com>
2021-12-20 11:23:41 +0900

    form付属partsコンポーネント化

--- src/components/buttons/readMoreButton.tsx
+++ src/components/buttons/readMoreButton.tsx
@@ -21,39 +21,33 @@ export default function ReadMoreButton({
   ...rest
 }: ButtonLinkProps) {
   return (
-    // <div className='w-full flex justify-center'>
-    //   <UnstyledLink
-    //     {...rest}
-    //     className={clsx(
-    //       'md:text-2xl py-2 md:py-4 px-4 md:px-8 inline-block rounded font-bold hover:text-green-400 animated-underline',
-    //       'border border-gray-700 shadow-sm',
-    //       'focus:outline-none focus-visible:text-primary-400',
-    //       {
-    //         'bg-dark text-white': variant === 'dark',
-    //         'bg-white text-dark hover:bg-gray-200 hover:text-dark focus-visible:text-dark border-gray-400':
-    //           variant === 'light',
-    //         'bg-primary-400 text-black hover:bg-primary-400/90 hover:text-black border-primary-500 focus-visible:text-dark':
-    //           variant === 'primary',
-    //       },
-    //       className
-    //     )}
-    //     style={
-    //       variant === 'primary'
-    //         ? ({
-    //             '--clr-primary-400': 'white',
-    //             '--clr-primary-500': 'white',
-    //           } as React.CSSProperties)
-    //         : undefined
-    //     }
-    //   >
-    //     {children}
-    //   </UnstyledLink>
-    // </div>
-    <>
-      <UnstyledLink href='/recruit' className="text-green-600 border border-green-600 rounded-3xl pt-2.5 pb-2 pl-3 text-sm">
+    <div className='w-full flex justify-center'>
+      <UnstyledLink
+        {...rest}
+        className={clsx(
+          'md:text-2xl py-2 md:py-4 px-4 md:px-8 inline-block rounded font-bold hover:text-green-400 animated-underline',
+          'border border-gray-700 shadow-sm',
+          'focus:outline-none focus-visible:text-primary-400',
+          {
+            'bg-dark text-white': variant === 'dark',
+            'bg-white text-dark hover:bg-gray-200 hover:text-dark focus-visible:text-dark border-gray-400':
+              variant === 'light',
+            'bg-primary-400 text-black hover:bg-primary-400/90 hover:text-black border-primary-500 focus-visible:text-dark':
+              variant === 'primary',
+          },
+          className
+        )}
+        style={
+          variant === 'primary'
+            ? ({
+                '--clr-primary-400': 'white',
+                '--clr-primary-500': 'white',
+              } as React.CSSProperties)
+            : undefined
+        }
+      >
         {children}
-        <span className="inline-block h-2 w-2 rounded-full ml-7 mr-2  mb-px bg-green-500"></span>
       </UnstyledLink>
-    </>
+    </div>
   );
 }
---
+++ src/molecules/form/submitButton.tsx
@@ -0,0 +1,8 @@
+export default function SubmitButton() {
+  return (
+    <input
+    type="submit"
+    className="text-lg leading-none text-white py-4 px-16 bg-green-500 rounded-3xl hover:bg-green-500 focus:ring-2 focus:ring-offset-2 focus:ring-green-600 focus:outline-none cursor-pointer"
+  />
+  )
+}

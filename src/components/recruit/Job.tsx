00c7af4bbc879c293fe3e9507c79c8445d0f96e8
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-20 16:40:52 +0900

    採用ページ修正

--- src/components/recruit/Job.tsx
+++ src/components/recruit/Job.tsx
@@ -13,8 +13,8 @@ export default function Job({categories}:{categories:any}){
   return(
     <>
       <section className='h-auto w-full'>
-      <h2 className='mt-12 text-2xl font-bold lg:text-4xl w-full  xl:pb-4 text-center'>Job infomation</h2>
-        <h3 className='text-xl lg:text-2xl w-full pb-2 xl:pb-8 text-center font-bold text-green-700'>- 求人情報 -</h3>
+      <h2 className=' text-2xl  lg:text-5xl w-full pb-2 lg:pb-6 text-center'>Job infomation</h2>
+        <h3 className='text-sm lg:text-2xl w-full pb-4 xl:pb-8 text-center text-green-700 font-YuGothic'>ー 求人情報 ー</h3>
         <div className="flex w-full justify-center">
           <div className="w-full lg:w-6/12 from-pink-50 to-indigo-100 place-items-center">
             <div className="w-full rounded py-12 md:py-0">

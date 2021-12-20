7d522b14aa254531f9038144a16a061c7eda3812
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-20 12:37:21 +0900

    コミット漏れ

--- src/components/top/Service.tsx
+++ src/components/top/Service.tsx
@@ -15,7 +15,7 @@ export default function Service(props:Props){
     <>
     <section className='bg-gray-50 h-auto'>
       <div className="">
-        <h2 className='text-2xl md:text-5xl w-full pb-4 text-center'>SERVICE</h2>
+        <h2 className='text-2xl md:text-5xl w-full pb-2 md:pb8 text-center'>SERVICE</h2>
         <h3 className='text-sm md:text-2xl w-full pb-11 xl:pb-16 text-center font-bold text-green-700 font-YuGothic'>ー 事業内容 ー</h3>
         <div className='w-full text-center space-y-8 xl:space-y-0 xl:space-x-32 md:flex md:justify-center py-18'>
           <ServiceContent image= {props.data.ecImage.url} title="Shopify構築" text = {text1}/>

7791d74741abc9085e5c3b4afb774eb47adef8cb
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-22 14:40:40 +0900

    Update suppliers.tsx

--- src/components/layout/suppliers.tsx
+++ src/components/layout/suppliers.tsx
@@ -11,6 +11,7 @@ export default function Suppliers(props: Props){
   return(
     <>
       <h2 className='text-2xl mb-14 md:text-4xl'>{props.topText}</h2>
+      
       <div className="text-center">
         <Image
           src={props.image}

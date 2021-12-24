9815d45dfdf34a51a5eafc4aeee98b8a38abb641
kaito040406 <kaitohasegawa000@gmail.com>
2021-12-24 15:28:33 +0900

    edit

--- src/components/top/Blog.tsx
+++ src/components/top/Blog.tsx
@@ -15,6 +15,7 @@ const mediaQueries = {
 export default function blog(props:Props){
   const isMobileSite = useMedia(mediaQueries.mobile);
   const isWide = useMedia({ maxWidth: "767px" });
+  const isWide2 = useMedia({ minWidth: "560px" });
 
   const blogCount=props.blogs.length
 
@@ -23,7 +24,19 @@ export default function blog(props:Props){
   if(isMobileSite){
     // スマホ処理
     blogHeight = Number(blogCount) * 300 + 190;
-    
+    console.log(isWide2)
+    if(isWide2){
+      // 各行の時
+      if(Number(blogCount) <= 2){
+        blogHeight = 175 + 225;
+      }else{
+        if(Number(blogCount) <= 2){
+          blogHeight = 175 + 425;
+        }else{
+          blogHeight = 175 + 725;
+        }
+      }
+    }
     if(Number(blogCount) == 0){
       blogHeight = 140;
     }
@@ -62,9 +75,9 @@ export default function blog(props:Props){
               <SectionTitle title="TOPIC" subTitle="トピック" position="left" />
 
               <div className="container px-5  max-w-7x1">
-                <div className="flex flex-wrap -m-4 md:mx-20">
+                <div className="flex flex-wrap -m-4 sm:mx-20">
                   {props.blogs.map((content:any) => (
-                    <div className="xl:w-1/3 md:w-1/2 p-4">
+                    <div className="xl:w-1/3 sm:w-1/2 p-4">
                       <UnstyledLink href={`/blog/${content.id}`} >
                         <div className="pt-2 pb-4 px-2 rounded-lg cursor-pointer">
                           <img 
@@ -75,11 +88,11 @@ export default function blog(props:Props){
                             alt={content.previewWord}
                             decoding="async" 
                           />
-                          <h3 className="tracking-widest text-dayColor text-sm md:text-xs font-medium mt-8 mb-2">
+                          <h3 className="tracking-widest text-dayColor text-sm sm:text-xs font-medium mt-8 mb-2">
                             <Moment format="YYYY.MM.DD" className="font-bold text-white font-YuGothic pt-1">
                               {content.createDay}
                             </Moment>
-                            <span className="mx-4 px-1 md:px-2 bg-yellow-main font-themeText rounded-sm text-white pb-0.5 pt-1">
+                            <span className="mx-4 px-1 sm:px-2 bg-yellow-main font-themeText rounded-sm text-white pb-0.5 pt-1">
                               {content.category.category}
                             </span>
                           </h3>
@@ -115,7 +128,7 @@ export default function blog(props:Props){
               <div className="container px-5  max-w-7x1">
                 <div className="flex flex-wrap -m-4">
                   {props.blogs.map((content:any) => (
-                    <div className="xl:w-1/3 md:w-1/2 p-4">
+                    <div className="xl:w-1/3 sm:w-1/2 p-4">
                       <UnstyledLink href={`/blog/${content.id}`} >
                         <div className="pt-2 pb-4 px-2 rounded-lg cursor-pointer">
                           <img 
@@ -126,11 +139,11 @@ export default function blog(props:Props){
                             alt={content.previewWord}
                             decoding="async" 
                           />
-                          <h3 className="tracking-widest text-dayColor text-sm md:text-xs font-medium mt-8 mb-2">
+                          <h3 className="tracking-widest text-dayColor text-sm sm:text-xs font-medium mt-8 mb-2">
                             <Moment format="YYYY.MM.DD" className="font-bold text-white font-YuGothic pt-1">
                               {content.createDay}
                             </Moment>
-                            <span className="mx-4 px-1 md:px-2 bg-yellow-main font-themeText rounded-sm text-white pb-0.5 pt-1">
+                            <span className="mx-4 px-1 sm:px-2 bg-yellow-main font-themeText rounded-sm text-white pb-0.5 pt-1">
                               {content.category.category}
                             </span>
                           </h3>

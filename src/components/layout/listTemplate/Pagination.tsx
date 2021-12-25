3d6128ae37e41a7b915a3303e4cbefff3a955884
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-12-25 15:22:37 +0900

    呪い解除

--- src/components/layout/listTemplate/Pagination.tsx
+++ src/components/layout/listTemplate/Pagination.tsx
@@ -20,7 +20,7 @@ export default function Pagination(props: Props){
       <PreviousPage pathName={props.pathName} pageNum={props.pageNum} />
       {range(1, Math.ceil(props.totalCount / PER_PAGE)).map((number:number, index:number) => (
         <span key={index} >
-        <TextColor pageNum={props.pageNum} num={number} pathName={props.pathName} totalCount={props.totalCount} pre_page={PER_PAGE} />
+        <TextColor pageNum={props.pageNum} num={number} pathName={props.pathName} totalCount={props.totalCount} pre_page={PER_PAGE}/>
         </span>
       ))}
       <NextPage pathName={props.pathName} pageNum={props.pageNum} totalCount={props.totalCount} pre_page={PER_PAGE} />

9ef7d36a51062f8c377b516b1e2d4491e4fe0fa6
ookuraseiya <seiya.ookura@gmail.com>
2022-02-10 15:51:39 +0900

    2/10 2/4西さん指摘箇所 URL位置変更、パートナー企業ページの追加途中
    ~HeaderMenu.tsx
      RECRUITとSERVISEの位置変更
    
    ~partner.tsx
      パートナー企業ページの追加するために作った。

---
+++ src/pages/partner.tsx
@@ -0,0 +1,12 @@
+import Seo from '@/components/Seo';
+import TopContent from '@/components/layout/TopContent';
+
+export default function Partner(props:any){
+
+  return(
+    <>
+      <Seo templateTitle='Partner' />
+      <TopContent title="PARTNER" text="パートナー企業一覧" />
+    </>
+  )
+}
